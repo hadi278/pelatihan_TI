629eb9ec1a3e34d613061eaae7b5cdbae7b5120c
hadi278 <hadirahmat712@gmail.com>
2022-07-18 10:36:02 +0700

    ini latihan node js

---
+++ src/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+
+
+const init = async()=>{
+    const server = Hapi.server({
+        host:process.env.HOST,
+        port: process.env.PORT,
+        routes:{
+            cors:{
+                origin:['*'],
+            },
+        },
+    });
+
+    server.route({
+        method: 'GET',
+        path :'/',
+        handler: ()=>({
+            name: 'Hadi Rachmaat Supindar',
+        }),
+    });
+
+    await server.start();
+    console.log(`server running at ${server.info.uri}`);
+};
+
+init();
